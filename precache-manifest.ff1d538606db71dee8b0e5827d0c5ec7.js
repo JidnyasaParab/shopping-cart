1860ef4fe1a599588a3886708aefae2cd5919a5c
Jidnyasa Parab <jidnyasacparab@gmail.com>
2020-07-28 19:33:20 +0530

    Updates

---
+++ precache-manifest.ff1d538606db71dee8b0e5827d0c5ec7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f854229c1d64b9ba3b914c47a305bd",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "bb6837b77c502ba04268",
+    "url": "/shopping-cart/static/css/main.8696bb88.chunk.css"
+  },
+  {
+    "revision": "f3c739d15917d430668d",
+    "url": "/shopping-cart/static/js/2.ff2e862f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart/static/js/2.ff2e862f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6837b77c502ba04268",
+    "url": "/shopping-cart/static/js/main.115afede.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "dc4632e588c378f59e9b23a51fad88c9",
+    "url": "/shopping-cart/static/media/foodcart.dc4632e5.jpg"
+  }
+]);
